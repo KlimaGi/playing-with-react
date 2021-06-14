fcd1b1a8bf3bb887ab86d4cd3037fba36aa6af35
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-14 18:18:02 +0300

    named import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js is running');
+// import { square, add } from './utils.js';
+import { isAdult, canDrink } from './person.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(4, 10));
+
+console.log(isAdult(17));
+console.log(canDrink(22));
