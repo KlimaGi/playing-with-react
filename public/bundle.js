afd75810ae3fe0b1269fc7b247ba1f07ff396fe2
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-14 18:39:08 +0300

    export default

--- public/bundle.js
+++ public/bundle.js
@@ -70,15 +70,17 @@
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
-// import { square, add } from './utils.js';
+ //import subtract, { square, add } from './utils.js';
 
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4, 10));
+//  console.log('app.js is running');
+//  console.log(square(4));
+//  console.log(add(4, 10));
+//  console.log(subtract(100, 83));
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](17));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](22));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](65));
 
 /***/ }),
 /* 1 */,
@@ -87,12 +89,14 @@ console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](22));
 
 "use strict";
 const isAdult = (age) => (age >= 18);
-/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
-
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
 
 const canDrink = (age) => (age >= 21 );
 /* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
+const isSenior = (age) => (age >= 65);
+
+/* harmony default export */ __webpack_exports__["b"] = (isSenior);
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-// import { square, add } from './utils.js';
-import { isAdult, canDrink } from './person.js';
+ //import subtract, { square, add } from './utils.js';
+import isSenior, { isAdult, canDrink } from './person.js';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4, 10));
+//  console.log('app.js is running');
+//  console.log(square(4));
+//  console.log(add(4, 10));
+//  console.log(subtract(100, 83));
 
 console.log(isAdult(17));
 console.log(canDrink(22));
+console.log(isSenior(65));
