643c4c0f930d341b6956c357c311029abd8e0cf5
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-16 19:23:13 +0300

    setting up webpack with scss

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
-
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-// added
-// yarn add react-modal@2.2.2 
